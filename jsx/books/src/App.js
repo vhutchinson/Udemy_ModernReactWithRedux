42d49c8678fef994bd750a27a4981e47e9be289e
Virginia Hutchinson <virginia_hutchinson@trimble.com>
2022-12-29 15:49:06 -0700

    Added local API server and HTTP calls to books project

--- jsx/books/src/App.js
+++ jsx/books/src/App.js
@@ -1,36 +1,54 @@
-import { useState } from 'react';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
 
 function App () {
   const [books, setBooks] = useState([]);
 
-  const generateId = () =>
-  {
-    return Math.round(Math.random() * 10000);
+  const getBooks = async () => {
+    var response = await axios.get("http://localhost:3001/books");
+
+    setBooks(response.data);
   };
 
-  const createBook = (title) => {
+  useEffect( () => {
+    getBooks();
+  }, []);
+
+  const createBook = async (title) => {
+    const response = await axios.post("http://localhost:3001/books", {
+      title: title
+    });
+
     const updatedBooks = [
       ...books,
-      {id: generateId(), title},
+      response.data,
     ];
 
     setBooks(updatedBooks);
   };
 
-  const deleteBookById = (id) => {
-    const updatedBooks = books.filter((book) => {
-      return book.id !== id
-    });
+  const deleteBookById = async (id) => {
+    const response = await axios.delete(`http://localhost:3001/books/${id}`);
     
-    setBooks(updatedBooks);
+    if (response.status === 200) {
+      const updatedBooks = books.filter((book) => {
+        return book.id !== id
+      });
+      
+      setBooks(updatedBooks);
+    }
   };
 
-  const editBookById = (id, newTitle) => {
+  const editBookById = async (id, newTitle) => {
+    const response = await axios.put(`http://localhost:3001/books/${id}`, {
+      title: newTitle
+    });
+
     const updatedBooks = books.map((book) => {
       if (book.id === id) {
-        return {...book, title: newTitle};
+        return {...book, ...response.data};
       }
 
       return book;
